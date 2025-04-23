5e2722f0b120397e139d92ecb9a8b712fef5971e
Yamlak Negash <yamlaknegash96@gmail.com>
2025-04-23 15:46:13 +0300

    Module 1 commit

---
+++ Module 1/String.py
@@ -0,0 +1,17 @@
+# Single and multi-line Strings.
+one1 = "this is a single line"
+two2 = "this is a multiple" \
+      "line string"
+
+print(one1)
+print(two2)
+
+#Concatenation.
+F_name = "Yamlak"
+N_name = "Negash"
+print(F_name + " " + N_name)
+
+
+print("Where do you live?")
+location = input()
+print("So you live in " + location)
---
+++ Module 1/hello_world.py
@@ -0,0 +1,7 @@
+print("Hello World")
+
+def say_hello(): 
+    print("Hello there!")
+    # return "Hello there!"; this this is missed it gives None.
+
+print(say_hello())
